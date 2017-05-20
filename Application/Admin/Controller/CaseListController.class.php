e087244ad6ec843c359fcaa9a424917c38b1d3ec
fengyingfb <446180102@qq.com>
2017-05-21 00:12:47 +0800

    caselist

--- Application/Admin/Controller/CaseListController.class.php
+++ Application/Admin/Controller/CaseListController.class.php
@@ -22,10 +22,11 @@ class CaseListController extends AdminController
     {   
         //model
         $dept_model = D('Common/Dept');
-        $uid = session('uid');
+        $uid = session('uid')?session('uid'):0;
         
         //搜索
         $action = I('post.action_type');
+        //查询
         $cond_where = array();
         if($action) {
             //dump(I());die;
@@ -42,6 +43,7 @@ class CaseListController extends AdminController
             $node_step = I('post.node_step', 0);
             $fromdate = I('post.fromdate', '');
             $enddate = I('post.enddate', '');
+            $case_status = I('post.case_status');
         }
 
         //实例化分页类 传入总记录数和每页显示的记录数
@@ -54,6 +56,7 @@ class CaseListController extends AdminController
         {   
             //待办
             case 'needToDo':{
+                $cond_where['type.status'] = array('IN', '1,2');
                 //搜索部门，并且是部门管理员
                 if (session('manage_privilege') == 1 && session('dept_id') > 0) {
                     $childDeptIds = $dept_model->getChildDeptIdsByDeptid(session('dept_id'));
@@ -62,7 +65,7 @@ class CaseListController extends AdminController
                     $cond_where['_string'] = " (case_list.accept_deptid IN (".$childDeptIds_str.") AND "
                             . " ( type.wfid is NULL OR type.wfid = 0) ) OR type.dept_id IN (".$childDeptIds_str.")";
                 } else {
-                    $cond_where['_string'] = " ((case_list.accept_uid = '".$uid."' AND (type.wfid is NULL OR type.wfid = 0)) OR type.handle_uid = '".$uid."' ";
+                    $cond_where['_string'] = " (case_list.accept_uid = '".$uid."' AND (type.wfid is NULL OR type.wfid = 0)) OR type.handle_uid = '".$uid."' ";
                     $cond_wf_child = " WHERE take_uid = '".$uid."' ";
                 }
                         $cond_where_child = "";
@@ -93,21 +96,18 @@ class CaseListController extends AdminController
                 }
                         //查询符合条件的总数
                 if($cond_where_child == "") {
-                    $total_num = $case_list_model->join('case_type AS type ON case_list.id = type.case_id')
+                    $total_num = $this->caseListModel->join('case_type AS type ON case_list.id = type.case_id')
                         ->where($cond_where)
                         ->count();
                 } else {
-                    $total_num = $case_list_model->join('case_type AS type ON case_list.id = type.case_id')
-                        ->join("(SELECT distinct(flowid) FROM org_workflow_step ".$cond_where_child." ) AS step ON type.wfid = step.flowid")
+                    $total_num = $this->caseListModel->join('case_type AS type ON case_list.id = type.case_id')
+                        ->join("(SELECT distinct(flow_id) FROM workflow_step ".$cond_where_child." ) AS step ON type.wfid = step.flow_id")
                         ->where($cond_where)
                         ->count();
                 }
 
                 $Page = new \Think\Page($total_num, $limit);
-                if($total_num>$listRows){
-                    $Page->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
-                }
-                
+
                 //查询列表数据
                 $search_field_arr = array(
                                 'case_list.*',
@@ -128,14 +128,14 @@ class CaseListController extends AdminController
                             );
                 $cond_order = array('type.handle_time' => 'desc');
                 if($cond_where_child == "") {
-                    $case_list_info = $case_list_model->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
+                    $case_list_info = $this->caseListModel->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
                     ->where($cond_where)
                     ->order($cond_order)
                     ->limit($Page->firstRow, $Page->listRows)
                     ->select();
                 } else {
-                    $case_list_info = $case_list_model->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
-                    ->join("(SELECT distinct(flowid) FROM org_workflow_step ".$cond_where_child." ) AS step ON type.wfid = step.flowid")
+                    $case_list_info = $this->caseListModel->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
+                    ->join("(SELECT distinct(flow_id) FROM org_workflow_step ".$cond_where_child." ) AS step ON type.wfid = step.flow_id")
                     ->where($cond_where)
                     ->order($cond_order)
                     ->limit($Page->firstRow, $Page->listRows)
@@ -146,15 +146,211 @@ class CaseListController extends AdminController
         
             //已办
             case 'done':{
+                $cond_where['type.status'] = array('IN', '1,2');
+                //时间、节点搜索
+                if((!empty($fromdate) || !empty($enddate)) && $node_step > 0 && $search_node_time_type != "") {
+                    if ( !empty($fromdate) && !empty($enddate) ) {
+                        $start_time = !empty($fromdate) ? strtotime($fromdate) : 0;
+                        $end_time = !empty($enddate) ? strtotime($enddate) + 86399 : 0;
+                    } else if (!empty($fromdate)) {
+                        $start_time = !empty($fromdate) ? strtotime($fromdate) : 0;
+                        $end_time = $start_time + 86399;
+                    } else {
+                        $end_time = !empty($enddate) ? strtotime($enddate) + 86399 : 0;
+                        $start_time = strtotime($enddate);
+                    }
+
+                    if ($search_node_time_type == 'submit_time') {
+                        $cond_where_node_time = '  AND submit_time between '. $start_time.' AND '.$end_time;
+                        $cond_where_node_time .= ' AND status IN (3,4)';
+                    } else if ($search_node_time_type == 'start_time') {
+                        $cond_where_node_time = '  AND accept_time between '. $start_time.' AND '.$end_time;
+                    }
+                    $cond_where_node_time .= ' AND node_id = '. $node_step;
+                    $cond_where_node_time .= ' AND isback = 0';
+                }else if ( (empty($fromdate) && empty($enddate)) && $node_step > 0) {
+                    $cond_where['type.wfnode'] = $node_step;
+                }
+            //没有搜索条件，并且是部门管理权限
+            if (session('manage_privilege') == 1 && session('dept_id') > 0) {
+                $uid_str = '';
+                $childDeptIds = $dept_model->getChildDeptIdsByDeptid(session('dept_id'));
+                $childDeptIds = array_merge($childDeptIds, array(0 => session('dept_id')));
+                $cond_user_where['dept_id'] = array('in', $childDeptIds);
+                $user_ids = M('user')->field('id')->where($cond_user_where)->select();
+                if (is_array($user_ids) && !empty($user_ids)) {
+                    $user_ids_arr = array();
+                    foreach ($user_ids as $key => $value) {
+                        $user_ids_arr[] = $value['id'];
+                    }
+                    $uid_str = implode(',', $user_ids_arr);
+                }
+
+                $cond_where_child = "WHERE take_uid IN (".$uid_str.") AND handle_type = 0 AND status IN (3,4) AND isvalid = 1";
+            } else {
+                $cond_where_child = "WHERE take_uid = '".$uid."' AND handle_type = 0 AND status IN (3,4) AND isvalid = 1";
+            }
+
+            //查询符合条件的总数
+            $total_num = $this->caseListModel->join('case_type AS type ON case_list.id = type.case_id')
+                            ->join("(SELECT DISTINCT(flow_id) FROM workflow_step WHERE "
+                                    . "flow_id IN (SELECT flow_id FROM workflow_step ".$cond_where_child." ) ". $cond_where_node_time." ) AS step ON "
+                                    . "type.wfid = step.flow_id")
+                            ->where($cond_where)
+                            ->count();
+            $Page = new \Think\Page($total_num, $limit);
+            $show = $Page->show();//分页显示输出
+
+            //查询列表数据
+            $search_field_arr = array(
+                            'case_list.*',
+                            'type.id AS type_id',
+                            'type.case_id',
+                            'type.case_type_id',
+                            'type.wfid',
+                            'type.wftype',
+                            'type.wfnode',
+                            'type.step_id',
+                            'type.dept_id',
+                            'type.handle_user_type',
+                            'type.handle_roleid',
+                            'type.handle_uid',
+                            'type.handle_time',
+                            'type.creattime',
+                            'type.status',
+                        );
+            $cond_order = array( 'type.handle_time' => 'desc' , 'type.creattime' => 'desc');
+            //查询列表数据
+            $case_list_info = $this->caseListModel->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
+                            ->join("(SELECT DISTINCT(flow_id) FROM workflow_step WHERE "
+                                    . "flow_id IN (SELECT flow_id FROM workflow_step ".$cond_where_child." ) ". $cond_where_node_time." ) AS step ON "
+                                    . "type.wfid = step.flow_id")
+                            ->where($cond_where)
+                            ->order($cond_order)
+                            ->limit($Page->firstRow, $Page->listRows)
+                            ->select();
                 $metaTitle = '已办案件';
             } break;
             
             //办结
             case 'end':{
+                $cond_where['type.status'] = empty($case_status) ? array('IN', '3,4') : $case_status;
+                    //时间、节点搜索
+                if((!empty($fromdate) || !empty($enddate)) && $node_step > 0 && $search_node_time_type != "") {
+                    if ( !empty($fromdate) && !empty($enddate) ) {
+                        $start_time = !empty($fromdate) ? strtotime($fromdate) : 0;
+                        $end_time = !empty($enddate) ? strtotime($enddate) + 86399 : 0;
+                    } else if (!empty($fromdate)) {
+                        $start_time = !empty($fromdate) ? strtotime($fromdate) : 0;
+                        $end_time = $start_time + 86399;
+                    } else {
+                        $end_time = !empty($enddate) ? strtotime($enddate) + 86399 : 0;
+                        $start_time = strtotime($enddate);
+                    }
+
+                    if ($search_node_time_type == 'submit_time') {
+                        $cond_where_node_time = '  AND submit_time BETWEEN '. $start_time.' AND '.$end_time;
+                        $cond_where_node_time .= ' AND status IN (3,4)';
+                    } else if ($search_node_time_type == 'start_time') {
+                        $cond_where_node_time = '  AND accept_time BETWEEN '. $start_time.' AND '.$end_time;
+                    }
+
+                    $cond_where_node_time .= ' AND node_id = '. $node_step;
+                    $cond_where_node_time .= ' AND isback = 0';
+                } else if ( (empty($fromdate) && empty($enddate)) && $node_step > 0) {
+                    $cond_where['type.wfnode'] = $node_step;
+                    $cond_where['string'] = " AND case_list.accept_uid = '".$uid."'";
+                }
+
+            //没有搜索条件，并且是部门管理权限
+            if (session('manage_privilege') == 1 && session('dept_id') > 0) {
+                $uid_str = '';
+                $childDeptIds = $dept_model->getChildDeptIdsByDeptid(session('dept_id'));
+                $childDeptIds = array_merge($childDeptIds, array(0 => session('dept_id')));
+                $cond_user_where['dept_id'] = array('in', $childDeptIds);
+                $user_ids = M('user')->field('id')->where($cond_user_where)->select();
+                if (is_array($user_ids) && !empty($user_ids)) {
+                    $user_ids_arr = array();
+                    foreach ($user_ids as $key => $value) {
+                        $user_ids_arr[] = $value['id'];
+                    }
+                    $uid_str = implode(',', $user_ids_arr);
+                }
+
+                $cond_where_child = "WHERE take_uid IN (".$uid_str.") AND handle_type = 0  AND isvalid = 1";
+            } else {
+                $cond_where_child = "WHERE take_uid = '".$uid."' AND handle_type = 0  AND isvalid = 1";
+            }
+            //查询符合条件的总数
+            $total_num = $this->caseListModel->join('case_type AS type ON case_list.id = type.case_id')
+                            ->join("(SELECT DISTINCT(flow_id) FROM workflow_step WHERE "
+                                    . "flow_id IN (SELECT flow_id FROM workflow_step ".$cond_where_child." ) ". $cond_where_node_time." ) AS step ON "
+                                    . "type.wfid = step.flow_id")
+                            ->where($cond_where)
+                            ->count();
+            $Page = new \Think\Page($total_num, $limit);
+            //查询列表数据
+            $search_field_arr = array(
+                            'case_list.*',
+                            'type.id AS type_id',
+                            'type.case_id',
+                            'type.case_type_id',
+                            'type.wfid',
+                            'type.wftype',
+                            'type.wfnode',
+                            'type.step_id',
+                            'type.dept_id',
+                            'type.handle_user_type',
+                            'type.handle_roleid',
+                            'type.handle_uid',
+                            'type.handle_time',
+                            'type.creattime',
+                            'type.status',
+                        );
+            $cond_order = array( 'type.handle_time' => 'desc' , 'type.creattime' => 'desc');
+            //查询列表数据
+            $case_list_info = $this->caseListModel->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
+                            ->join("(SELECT DISTINCT(flow_id) FROM workflow_step WHERE "
+                                    . "flow_id IN (SELECT flow_id FROM workflow_step ".$cond_where_child." ) ". $cond_where_node_time." ) AS step ON "
+                                    . "type.wfid = step.flow_id")
+                            ->where($cond_where)
+                            ->order($cond_order)
+                            ->limit($Page->firstRow, $Page->listRows)
+                            ->select();
                 $metaTitle = '办结案件';
             } break;
             //全公司
             case 'all':{
+            //查询符合条件的总数
+            $total_num = $this->caseListModel->join('case_type AS type ON case_list.id = type.case_id')
+                            ->where($cond_where)
+                            ->count();
+            $Page = new \Think\Page($total_num, $limit);
+            //查询列表数据
+            $search_field_arr = array(
+                            'case_list.*',
+                            'type.id AS type_id',
+                            'type.case_id',
+                            'type.case_type_id',
+                            'type.wfid',
+                            'type.wftype',
+                            'type.wfnode',
+                            'type.step_id',
+                            'type.dept_id',
+                            'type.handle_user_type',
+                            'type.handle_roleid',
+                            'type.handle_uid',
+                            'type.handle_time',
+                            'type.creattime',
+                            'type.status',
+                        );
+            $cond_order = array( 'type.handle_time' => 'desc' , 'type.creattime' => 'desc');
+            //查询列表数据
+            $case_list_info = $this->caseListModel->field($search_field_arr)->join('case_type AS type ON case_list.id = type.case_id')
+                            ->where($cond_where)
+                            ->order($cond_order)
+                            ->limit($Page->firstRow, $Page->listRows)
+                            ->select();
                 $metaTitle = '所有案件';
             } break;
         }
@@ -164,37 +360,41 @@ class CaseListController extends AdminController
         $confLoanType = $this->caseListModel->getLoanType();
         //案例状态
         $confCaseStatus = $this->caseListModel->getConfCaseStatus();
-        /***流程类型***/
-        $flowType = new \Admin\Model\FlowTypeModel();
-        $conf_where['category'] = 1;
-        $confFlowTypeTemp = $flowType->getFlowTypeByConf($conf_where);
+//        /***流程类型***/
+//        $flowType = new \Admin\Model\FlowTypeModel();
+//        $conf_where['category'] = 1;
+//        $confFlowTypeTemp = $flowType->getFlowTypeByConf($conf_where);
+//        
+//        /***流程类型数组***/
+//        $confFlowType = array();
+//        if(is_array($confFlowTypeTemp) && !empty($confFlowTypeTemp))
+//        {
+//            foreach($confFlowTypeTemp as $key => $value)
+//            {
+//                $confFlowType[$value['id']] = $value['type_name'];
+//            }
+//        }
+//        
+//        /***流程节点数组***/
+//        $flowConfType = new \Admin\Model\FlowConfModel();
+//        $confFlowNodeTemp = $flowConfType->getFlowNodeByConf();
+//        $confFlowNode = array();
+//        if(is_array($confFlowNodeTemp) && !empty($confFlowNodeTemp))
+//        {
+//            foreach($confFlowNodeTemp as $key => $value)
+//            {
+//                $confFlowNode[$value['id']] = $value['node_name'];
+//            }
+//        }
         
-        /***流程类型数组***/
-        $confFlowType = array();
-        if(is_array($confFlowTypeTemp) && !empty($confFlowTypeTemp))
-        {
-            foreach($confFlowTypeTemp as $key => $value)
-            {
-                $confFlowType[$value['id']] = $value['type_name'];
-            }
+        
+        
+        if($total_num>$listRows){
+            $Page->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
         }
-        
-        /***流程节点数组***/
-        $flowConfType = new \Admin\Model\FlowConfModel();
-        $confFlowNodeTemp = $flowConfType->getFlowNodeByConf();
-        $confFlowNode = array();
-        if(is_array($confFlowNodeTemp) && !empty($confFlowNodeTemp))
-        {
-            foreach($confFlowNodeTemp as $key => $value)
-            {
-                $confFlowNode[$value['id']] = $value['node_name'];
-            }
-        }
-        
-        
         $show = $Page->show();
         $this->assign('_Page', $show ? $show : '');
-        $this->assign('_Total',$total);
+        $this->assign('_Total',$total_num);
         $this->assign('confBusinessType',$confBusinessType);
         $this->assign('confLoanType',$confLoanType);
         $this->assign('confCaseStatus',$confCaseStatus);
